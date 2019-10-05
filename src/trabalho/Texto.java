fbc067b5d8648364c6ce18196600aa0d83a90e70
Gabriela Vidal <ga28bvidal@gmail.com>
2019-10-05 15:30:55 -0300

    inserir

--- src/trabalho/Texto.java
+++ src/trabalho/Texto.java
@@ -17,39 +17,70 @@ public class Texto {
 	private final Scanner in = new Scanner(System.in);	
 
 	public Texto(){
-		lista.add("kk ABCDEFGHIJKLMNOPQRSTUVWXYZ Afersons");	
+            lista.add("kk ABCDEFGHIJKLMNOPQRSTUVWXYZ Afersons");	
 	}
 
-
+        public void insere(){
+            String palavra;
+            int tamanhoLista=lista.size()-1;
+            String atual;
+            int tamanhoTot;
+            int addLista=800;
+            
+            palavra = in.nextLine();
+            atual = lista.get(tamanhoLista);
+            tamanhoTot = atual.length() + palavra.length();
+            
+            while(tamanhoTot>400){
+                while(addLista/400>1){
+                    if(palavra.length()>400){
+                        addLista = palavra.length() - 400;
+                    }else{
+                        addLista = 400 - palavra.length();
+                    }
+                }
+                tamanhoTot = tamanhoTot-atual.length();
+                atual = atual + palavra.substring(0, addLista);
+                lista.add(tamanhoLista, atual);
+                palavra = palavra.substring(addLista, tamanhoTot+1);
+                tamanhoTot = atual.length() + palavra.length();
+            }
+            
+            if(palavra.length()!=0){
+                lista.add(palavra);
+            }
+            
+        }
+        
 	public String remove(){
-		int i;
-		int pos = lista.size()-1;
-		int length = lista.get(pos).length();
-		String ret="";
+            int i;
+            int pos = lista.size()-1;
+            int length = lista.get(pos).length();
+            String ret="";
 
-		i=in.nextInt();
+            i=in.nextInt();
 
-		while(true){
-			if(length>i){
-				ret=lista.get(pos).substring(length-i,length-1);
-				lista.set(pos,lista.get(pos).substring(0,length-i-1));
-				return ret;
-			}else if(length==i){
-				ret=lista.get(pos);
-				lista.remove(pos);
-				return ret;
-			}else{
-				ret=ret.concat(lista.get(pos));
-				if(pos!=0){
-					lista.remove(pos);
-					i-=length;
-					pos--;
-				}else{
-					lista.set(pos,"");
-					i=0;
-				}
-			}
-		}
+            while(true){
+                    if(length>i){
+                            ret=lista.get(pos).substring(length-i,length-1);
+                            lista.set(pos,lista.get(pos).substring(0,length-i-1));
+                            return ret;
+                    }else if(length==i){
+                            ret=lista.get(pos);
+                            lista.remove(pos);
+                            return ret;
+                    }else{
+                            ret=ret.concat(lista.get(pos));
+                            if(pos!=0){
+                                    lista.remove(pos);
+                                    i-=length;
+                                    pos--;
+                            }else{
+                                    lista.set(pos,"");
+                                    i=0;
+                            }
+                    }
+            }
 	}
 
 	public void print(){
